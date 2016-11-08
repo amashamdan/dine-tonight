cfbe7f96193f3a3d70a7bab4f76ddd00ae6c3b64
Amer Hamdan <amashamdan@gmail.com>
2016-11-08 11:50:10 -0800

    Feat: The user can now perform a search based on location and get results in json.
    The user can search restaurents in certain location based on rating or distance.
    The user receives the results in json format.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var ejs = require("ejs");
 var mongodb = require("mongodb");
+var Yelp = require("yelp");
 var passport = require('passport');
 var Strategy = require('passport-facebook').Strategy;
+var bodyParser = require("body-parser");
+var parser = bodyParser.urlencoded({extended: false});
 
 var app = express();
 var MongoClient = mongodb.MongoClient;
@@ -11,6 +14,13 @@ var mongoUrl = process.env.NIGHTLIFE;
 app.use("/stylesheets", express.static(__dirname + "/views/stylesheets"));
 app.use("/scripts", express.static(__dirname + "/views/scripts"));
 
+var yelp = new Yelp({
+	consumer_key: process.env.CONSUMER_KEY,
+	consumer_secret: process.env.CONSUMER_SECRET,
+	token: process.env.TOKEN,
+	token_secret: process.env.TOKEN_SECRET
+});
+
 MongoClient.connect(mongoUrl, function(err, db) {
 	if (err) {
 		res.end("Failed to connect to Database.")
@@ -18,7 +28,21 @@ MongoClient.connect(mongoUrl, function(err, db) {
 		var restaurents = db.collection("restaurents");
 		app.get("/", function(req, res) {
 			res.render("index.ejs");
-		})
+		});
+
+		app.post("/results", parser, function(req, res) {
+			var sorting = Number(req.body.sorting);
+			yelp.search({
+				term: "restaurant", 
+				location: req.body.location,
+				limit: 20,
+				sort: sorting
+			}).then(function(data) {
+				res.send(data);
+				res.end();
+			});
+			//res.render("results.ejs")
+		});
 	}
 });
 
