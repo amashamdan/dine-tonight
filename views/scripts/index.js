47d2b7c1b9ac977f82df141ba60d97c0713997f2
Amer Hamdan <amashamdan@gmail.com>
2016-11-09 12:52:01 -0800

    Feat: Find me button now operational.
    The button will detect the user's location using ipinfo.io.
    Then the form is submitted with the detected location.

--- views/scripts/index.js
+++ views/scripts/index.js
@@ -7,4 +7,19 @@ $(document).ready(function() {
 			e.target.submit();
 		}
 	})
+	$("#find-me-button").click(function(e) {
+		e.preventDefault();
+
+		/* Location lookup using ipinfo api */
+		$.getJSON('http://ipinfo.io', function(data){
+			/* The location is stored in the variable location. */
+			console.log(data);
+			var location = data.city + " " + data.region;
+			$("#search-input").val(location);
+			// e.target.submit() doesn't work, seems like problem referencing the form.
+			$("#search-form").submit();
+		});
+	});
 });
+
+
