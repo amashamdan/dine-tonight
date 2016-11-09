cf6e866e96076a7d4d0f76526cbfc7e5039ef461
Amer Hamdan <amashamdan@gmail.com>
2016-11-09 11:12:55 -0800

    Feat: Added event handler for clicking Expand information button.
    The handler fadeToggles the more-info div which contains additional information about a restaurant.

--- views/scripts/results.js
+++ views/scripts/results.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
-	console.log("HI")
-	var counter = 0;
-	$(".result").each(function(i) {
-		var result = $(this);
-		setTimeout(function() {
-			$(result).slideDown(300);
-		}, 300 * counter);
-		counter++;
-	});
+	$(".results-div").slideDown(800);
+	$(".more-info-button").click(function() {
+		$(this).parent().siblings(".more-info").fadeToggle();
+	})
 });
