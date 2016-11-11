370b6f48e7c1128ce1ad85393aa8dd1a3c4387e6
unknown <amashamdan@gmail.com>
2016-11-11 14:16:21 -0800

    Refactor: Removed region from location.
    Find me button returned city and region which were saved together as location.
    Region is moved now to avoid confusion in the database in case the user enters city alone.
    For exmaple, if user in Seattle and searches Seattle, location is 'seattle'.
    If using find me it will be 'seattle washington'. So depending on how search is done, info retreival from database may differ.

--- views/scripts/index.js
+++ views/scripts/index.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 		/* Location lookup using ipinfo api */
 		$.getJSON('https://ipinfo.io', function(data){
 			/* The location is stored in the variable location. */
-			var location = data.city + " " + data.region;
+			var location = data.city;
 			$("#search-input").val(location);
 			// e.target.submit() doesn't work, seems like problem referencing the form.
 			$("#search-form").submit();
