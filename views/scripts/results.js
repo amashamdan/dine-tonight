9ce3913426e56754a4b18de641e047e4c4cdb331
unknown <amashamdan@gmail.com>
2016-11-11 14:18:25 -0800

    Feat: will go link's href is changed.
    Each 'Iwant to go' button is enclosed inside a link. The link will /action/:the-restaurants-name.
    This is done here not in ejs directly.

--- views/scripts/results.js
+++ views/scripts/results.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+	$(".will-go").each(function(i) {
+		$(this).parent().attr("href", "/action/" + $(this).parents(".result").attr("id"));
+	});
 	$(".results-div").slideDown(800);
 	$(".more-info-button").click(function() {
 		$(this).parent().siblings(".more-info").fadeToggle();
