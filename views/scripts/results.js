5ec027fc6d08067f5c68da124c3f310509aab604
unknown <amashamdan@gmail.com>
2016-11-11 15:10:53 -0800

    Feat: href added to the 'Don't wanna go' link.
    The link enclosing the button has href which is changed to /cancel/:place.

--- views/scripts/results.js
+++ views/scripts/results.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
 	$(".will-go").each(function(i) {
 		$(this).parent().attr("href", "/action/" + $(this).parents(".result").attr("id"));
 	});
+	$(".will-not-go").each(function(i) {
+		$(this).parent().attr("href", "/cancel/" + $(this).parents(".result").attr("id"));
+	});
 	$(".results-div").slideDown(800);
 	$(".more-info-button").click(function() {
 		$(this).parent().siblings(".more-info").fadeToggle();
